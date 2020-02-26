a9e7c8dff60da24ec1c4503ed5b1ed6b1a7d6c78
Wissam Sawah <wesam.sawah@me.com>
2020-02-26 12:02:01 +0100

    Bash script modified

--- static_bash.bash
+++ static_bash.bash
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 #
 #
-# Assuming that you have the plugin under the Horizon directory you will be able to use this script.
-
 
 # USE this if you installed the plugin under horizon/openstack_dashboard/dashboards/.
 
-rsync -av policy/cafe_ui/static/dashboard/cafe ../../../../../static/dashboard
+rsync -av policy/cafe_ui/static/dashboard/cafe ../../../static/dashboard
