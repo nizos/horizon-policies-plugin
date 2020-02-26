38fbd73325a68f784cadbee7e19889a2115d1ee8
Wissam Sawah <wesam.sawah@me.com>
2020-02-26 11:26:34 +0100

    Bash script updated with instr

--- static_bash.bash
+++ static_bash.bash
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #
 #
-#
+# Assuming that you have the plugin under the Horizon directory you will be able to use this script.
 # copy static from the plugin to Horizon/static
 rsync -av policy/cafe_ui/static/dashboard/cafe ../../../static/dashboard
