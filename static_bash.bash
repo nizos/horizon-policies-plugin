09529a97c54671e112c0d213f498703775830f6e
Wissam Sawah <wesam.sawah@me.com>
2020-02-26 11:43:38 +0100

    Bash script updated with 2 paths

--- static_bash.bash
+++ static_bash.bash
@@ -2,5 +2,11 @@
 #
 #
 # Assuming that you have the plugin under the Horizon directory you will be able to use this script.
-# copy static from the plugin to Horizon/static
-rsync -av policy/cafe_ui/static/dashboard/cafe ../../../static/dashboard
+
+# copy static from the plugin to Horizon/static. Use this if you installed the plugin under /Horizion ***NOT DASHBOARDS"
+#rsync -av policy/cafe_ui/static/dashboard/cafe ../../../static/dashboard
+
+
+# USE this if you installed the plugin under horizon/openstack_dashboard/dashboards/.
+
+rsync -av policy/cafe_ui/static/dashboard/cafe ../../../../../static/dashboard
