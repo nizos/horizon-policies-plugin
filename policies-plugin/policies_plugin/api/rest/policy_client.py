fb24918fce30990b84f5ef2c2f761b2d92914d3e
Rickard <Garnnok@gmail.com>
2020-03-27 15:18:18 +0100

    Created function that retrieves policies from keystone in backend
    Writes policies to log file

--- policies-plugin/policies_plugin/api/rest/policy_client.py
+++ policies-plugin/policies_plugin/api/rest/policy_client.py
@@ -10,10 +10,15 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import logging
+import itertools
+
 from django.views import generic
 from openstack_dashboard.api.rest import urls
 from openstack_dashboard.api.rest import utils as rest_utils
 from policies_plugin.api.rest.policy_test_api import get_text
+LOG = logging.getLogger(__name__)
+
 
 @urls.register
 class PolicyClient(generic.View):
@@ -21,10 +26,21 @@ class PolicyClient(generic.View):
 
     @rest_utils.ajax()
     def get(self, request):
+        list_of_policies = []
         list_of_policies = get_text()
+        test_list = list_of_policies.items()
+        another_test_list = []
+        for items in test_list:
+          another_test_list.append(items[1])
+        LOG.warning("##############################################")
+        LOG.warning("!HERE client!")
+        for i in list(itertools.chain.from_iterable(another_test_list)):
+          LOG.warning(i)
+        LOG.warning("##############################################")
+        #test_list = list_of_policies.items()
         greeting = {
           'id': 'Test',
-          'name': list_of_policies[0]
+          'name': test_list[3]
         }
         #greeting = {
         #  'id': 'Hello',
