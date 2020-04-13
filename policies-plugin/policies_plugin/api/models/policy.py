5832ed9d06921ec27a75db1dd25885f9b742d9aa
nizos <nizar.selander@gmail.com>
2020-04-13 16:02:28 +0200

    Cleaned up and simplified back end api, client and policy model
    The client and the api code has been cleaned up and so has the policy model. The output file from oslo.policy is now created and removed for each operation.

---
+++ policies-plugin/policies_plugin/api/models/policy.py
@@ -0,0 +1,119 @@
+import logging
+from policies_plugin.api.resources.keystone_fields import keystone_docs
+LOG = logging.getLogger(__name__)
+
+# Policy Class
+class Policy:
+    def __init__(self,
+                project="Project",
+                target="Target",
+                rule="Rule",
+                default="Default rule",
+                scopes="Scopes",
+                operations="Operations",
+                description="Description"):
+        self.project = project
+        self.target = target
+        self.rule = rule
+        self.default = default
+        self.scopes = scopes
+        self.operations = operations
+        self.description = description
+
+    def from_line(self, line):
+        line_formatted = self.strip_new_line(self.strip_quotes(line))
+        line_target = line_formatted.split('": "')[0]
+        self.set_project(line_target)
+        self.set_target(line_target)
+        self.set_rule(line_formatted)
+        policy_dict = self.get_dict()
+        self.set_default(policy_dict)
+        self.set_scopes(policy_dict)
+        self.set_operations(policy_dict)
+        self.set_description(policy_dict)
+        return self
+
+
+    # SETTERS
+    def get_dict(self):
+        policy_dict = ""
+        try:
+            policy_dict = keystone_docs[self.create_identifier()]
+        except KeyError:
+            policy_dict = None
+        return policy_dict
+
+
+    def create_identifier(self):
+        identifier = ""
+        if (self.project != "global"):
+            identifier = self.project + ":" + self.target
+        else:
+            identifier = self.target
+        return identifier
+
+
+    def set_project(self, line_target):
+        if ':' in line_target:
+            self.project = line_target.split(':')[0]
+        else:
+            self.project = "global"
+
+    def set_target(self, line_target):
+        if ':' in line_target:
+            self.target = line_target.split(':')[1]
+        else:
+            self.target = line_target
+
+    def set_rule(self, line_stripped_new_line):
+        if '": "' in line_stripped_new_line:
+            self.rule = line_stripped_new_line.split('": "')[1]
+        else:
+            self.rule = "None"
+            LOG.error("No rule provided for requested policy target: {}".format(line_stripped_new_line))
+
+    def set_default(self, policy_dict):
+        try:
+            self.default = policy_dict["default"]
+        except:
+            self.default = "No default"
+
+    def set_scopes(self, policy_dict):
+        try:
+            self.scopes = policy_dict["scopes"]
+        except:
+            self.scopes = "No scopes"
+
+    def set_operations(self, policy_dict):
+        try:
+            self.operations = policy_dict["operations"]
+        except:
+             self.operations = "No operations"
+
+    def set_description(self, policy_dict):
+        try:
+            self.description = policy_dict["description"]
+        except:
+            self.description = "No description"
+
+     # GETTERS
+    def to_json(self):
+        policy_json = {
+                'project': self.project,
+                'target': self.target,
+                'rule': self.rule,
+                'default': self.default,
+                'scopes': self.scopes,
+                'operations': self.operations,
+                'description': self.description
+        }
+        return policy_json
+
+    # HELPERS
+    def strip_quotes(self, input_text):
+        stripped = input_text[1:-1]
+        return stripped
+
+    def strip_new_line(self, input_text):
+        stripped = input_text.rstrip()
+        return stripped
--- policies-plugin/policies_plugin/api/rest/policy_client.py
+++ policies-plugin/policies_plugin/api/rest/policy_client.py
@@ -1,114 +1,46 @@
 from oslo_policy import generator
-from policies_plugin.models.policy import Policy
-from policies_plugin.api.rest.keystone_fields import keystone_docs
+from policies_plugin.api.models.policy import Policy
+import os
 import logging
 
 LOG = logging.getLogger(__name__)
 
 class Policy_Client:
 
+    FILE_PATH = "/opt/stack/horizon/horizon-policies-plugin/" + "policies-plugin/policies_plugin/api/resources/policy_output.txt"
+
     def get_policies(self):
-        FILE_PATH = "/opt/stack/horizon/horizon-policies-plugin/" + "policies-plugin/policies_plugin/api/rest/policy_output.txt"
-
-        generator._generate_policy("keystone", FILE_PATH)
-
-        list_of_policies = []
-        with open(FILE_PATH) as file:
-            for line in file:
-                list_of_policies.append(line.rstrip())
-
-        policy_items = []
-        for policy in list_of_policies:
-            policy_item = Policy()
-            formatted = policy[1:-1]
-            formatted = formatted.rstrip()
-            target = formatted.split('": "')[0]
-
-            if ':' in target:
-                # Extract the project and target from the target string
-                policy_item.project = target.split(':')[0]
-                policy_item.target = target.split(':')[1]
-            else:
-                # In case it did not have any project (and likely were a rule), set it to Global instead
-                policy_item.project = "Global"
-                policy_item.target = target
-
-            try:
-                # Attempt to get the default values from the dict
-                policy_item.default = keystone_docs[target]["default"]
-                policy_item.scopes = keystone_docs[target]["scopes"]
-                policy_item.operations = keystone_docs[target]["operations"]
-                policy_item.description = keystone_docs[target]["description"]
-            except KeyError:
-                # In case there was no defaults, set up the values for a home made policy.
-                policy_item.default = policy_item.target
-                policy_item.scopes = "HOME_BREW" # FIXME - There was another way to get scopes/targets?
-                policy_item.operations = "HOME_BREW" # FIXME - There was another way to get scopes/targets?
-                policy_item.description = "Non-default policy, made by the user"
-
-            policy_item.rule = formatted.split('": "')[1]
-
-
-
-            policy_items.append(policy_item)
-
-        return policy_items
+        policy_lines = self.keystone_get_policies()
+        policies = []
+        for line in policy_lines:
+            policies.append(Policy().from_line(line))
+        return policies
 
     def get_policy(self, request, project, target):
-        LOG.warning("Get policy called in Policy API")
-        LOG.warning("identity:"+target)
-        FILE_PATH = "/opt/stack/horizon/horizon-policies-plugin/" + "policies-plugin/policies_plugin/api/rest/policy_output.txt"
-        generator._generate_policy("keystone", FILE_PATH)
+        policy_line = self.keystone_get_policy(project, target)
+        return Policy().from_line(policy_line)
 
-        requested = ""
-        if (project != "Global"):
-            requested = project+":"+target
-        else:
-            requested = target
-
-        LOG.warning("requested: " + requested)
-        policy_line = requested
-        with open(FILE_PATH) as file:
+    def keystone_get_policies(self):
+        generator._generate_policy("keystone", self.FILE_PATH)
+        list_of_policies = []
+        with open(self.FILE_PATH) as file:
             for line in file:
-                if requested in line:
-                    policy_line = line.rstrip()
-                    LOG.warning("Policy found in Policy API: " + policy_line)
+                list_of_policies.append(line.rstrip())
+        os.remove(self.FILE_PATH)
+        return list_of_policies
 
-        policy_item = Policy()
-        formatted = policy_line[1:-1]
-        formatted = formatted.rstrip()
-        target = formatted.split('": "')[0]
+    def keystone_get_policy(self, project, target):
+        generator._generate_policy("keystone", self.FILE_PATH)
 
-        if ':' in target:
-            # Extract the project and target from the target string
-            policy_item.project = target.split(':')[0]
-            policy_item.target = target.split(':')[1]
+        identifier = ""
+        if (project != "global"):
+            identifier = project+":"+target
         else:
-            # In case it did not have any project (and likely were a rule), set it to Global instead
-            policy_item.project = "Global"
-            policy_item.target = target
-
-        try:
-            # Attempt to get the default values from the dict
-            policy_item.default = keystone_docs[target]["default"]
-            policy_item.scopes = keystone_docs[target]["scopes"]
-            policy_item.operations = keystone_docs[target]["operations"]
-            policy_item.description = keystone_docs[target]["description"]
-        except KeyError:
-            # In case there was no defaults, set up the values for a home made policy.
-            policy_item.default = policy_item.target
-            policy_item.scopes = "HOME_BREW" # FIXME - There was another way to get scopes/targets?
-            policy_item.operations = "HOME_BREW" # FIXME - There was another way to get scopes/targets?
-            policy_item.description = "Non-default policy, made by the user"
-
-        policy_item.rule = formatted.split('": "')[1]
-
-        LOG.warning("Policy API returning policy.project: " + repr(policy_item.project))
-        LOG.warning("Policy API returning policy.target: " + repr(policy_item.target))
-        LOG.warning("Policy API returning policy.rule: " + repr(policy_item.rule))
-        LOG.warning("Policy API returning policy.default: " + repr(policy_item.default))
-        LOG.warning("Policy API returning policy.scopes: " + repr(policy_item.scopes))
-        LOG.warning("Policy API returning policy.operations: " + repr(policy_item.operations))
-        LOG.warning("Policy API returning policy.description: " + repr(policy_item.description))
-
-        return policy_item
+            identifier = target
+        policy_line = ""
+        with open(self.FILE_PATH) as file:
+            for line in file:
+                if identifier in line:
+                    policy_line = line.rstrip()
+        os.remove(self.FILE_PATH)
+        return policy_line
