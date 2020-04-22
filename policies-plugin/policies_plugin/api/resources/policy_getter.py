5461ff48c5e280c0bf59534fbafbf60138a430e5
Joel Amundberg <joel.amundberg@gmail.com>
2020-04-22 10:46:29 +0200

    Initial error handling for policy verification
    The policy will now throw a 400-response back to the front-end if the policy does not look like it is valid. This is the first very basic step in handling syntax verification, but it's something usable.

--- policies-plugin/policies_plugin/api/resources/policy_getter.py
+++ policies-plugin/policies_plugin/api/resources/policy_getter.py
@@ -1,4 +1,6 @@
-from oslo_policy import generator, policy
+from oslo_policy import generator, policy, _parser
+from django.http import HttpResponse
+
 import json
 import logging
 
@@ -54,10 +56,38 @@ def local_set_policy(namespace, updateRule):
         for rule in rule_defaults:
             policy_dict[rule.name] = rule.check_str
 
+    try:
+        val = throwing_parse_check("\""+ updateRule['rule']+"\"")
+    except ValueError as e:
+        resp = HttpResponse()
+        resp.status_code = 400
+        resp.content = str(e)
+        return resp
+
     policy_dict[identifier] = updateRule['rule']
 
     #FIXME - Path should not be hardcoded - /etc/keystone/
     with open('/etc/keystone/policy.json', 'w') as policyfile:
-        json.dump(policy_dict, policyfile)
+        json.dump(policy_dict, policyfile, sort_keys=True, indent=4, separators=(',', ': '))
 
-    return 200
+    resp = HttpResponse()
+    resp.status_code = 200
+    return resp
+
+def throwing_parse_check(rule):
+    try:
+        kind, match = rule.split(':', 1)
+    except Exception:
+        raise ValueError('Failed to understand rule %s', rule)
+
+    # Find what implements the check
+    extension_checks = _parser._checks.get_extensions()
+    if kind in extension_checks:
+        return extension_checks[kind](kind, match)
+    elif kind in _parser._checks.registered_checks:
+        return _parser._checks.registered_checks[kind](kind, match)
+    elif None in _parser._checks.registered_checks:
+        return _parser._checks.registered_checks[None](kind, match)
+    else:
+        raise ValueError('No handler for matches of kind %s', kind)
+
