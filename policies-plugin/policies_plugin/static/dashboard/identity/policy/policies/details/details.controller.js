8eefd440e0db44b1d07807a588efe3247d65e814
Andreas <drogo681@gmail.com>
2020-04-29 15:36:11 +0200

    Added some missing suggestions for autocomplete.

--- policies-plugin/policies_plugin/static/dashboard/identity/policy/policies/details/details.controller.js
+++ policies-plugin/policies_plugin/static/dashboard/identity/policy/policies/details/details.controller.js
@@ -14,7 +14,7 @@
                 $scope.dirty = {};
                 $scope.resultsToShow = 5;
 
-                var suggestions = ["admin", "admin_required", "rule:admin_required", "rule:service_role", "rule:service_or_admin", "rule:token_subject", "target.credential.user_id", "target.domain.id", "target.domain_id", "target.group.domain_id", "target.limit.domain.id", "target.limit.project.domain_id", "target.limit.project_id", "target.trust.trustor_user_id", "target.trust.trustee_user_id", "target.project.domain_id", "target.project.id", "target.role.domain_id", "target.token.user_id", "target.user.id", "target.user.domain_id", "token.domain.id", "token.project.domain.id"];
+                var suggestions = ["admin", "admin_required", "rule:admin_required", "rule:service_role", "rule:service_or_admin", "rule:token_subject", "target.credential.user_id", "target.domain.id", "target.domain_id", "target.group.domain_id", "target.limit.domain.id", "target.limit.project.domain_id", "target.limit.project_id", "target:credential.user_id", "target.trust.trustor_user_id", "target.trust.trustee_user_id", "target.user.id", "target.project.domain_id", "target.project.id", "target.role.domain_id", "target.token.user_id", "target.user.domain_id", "token.domain.id", "token.project.domain.id", "role:reader", "role:admin", "and", "or", "rule:owner", "system_scope:all"];
 
                 $scope.ok = function(policy) {
                     $uibModalInstance.close(policy);
